34420efbf53dc30a731db4a498fc5384a6702bfd
hagoy00 <hagos_y@icloud.com>
2026-04-20 21:57:35 -0700

    Add Streamlit app for Surplus/Deficit analysis

--- Surplus_Deficit.py
+++ Surplus_Deficit.py
@@ -1,1 +1,15 @@
+import streamlit as st
+from utils.db_utils import load_data
+from utils.data_utils import extract_subtotals
+
+st.title("Surplus / Deficit")
+
+df = load_data()
+subtotals = extract_subtotals(df)
+
+pivot = subtotals.pivot_table(
+    index="Year", columns="Category", values="Amount", aggfunc="sum"
+)
+
+st.dataframe(pivot)
 
