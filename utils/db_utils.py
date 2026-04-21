74d4d0e947bb054a922971f5ee5263324934f096
hagoy00 <hagos_y@icloud.com>
2026-04-21 11:48:18 -0700

    Update db_utils.py

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -1,10 +1,8 @@
-
 import pandas as pd
 
 def load_data():
     """
     Loads the MECCA Excel file as a DataFrame.
-    Assumes the file is in the project root folder.
     """
     file_path = "MECCA_Financial_Data.xlsx"
     df = pd.read_excel(file_path)
