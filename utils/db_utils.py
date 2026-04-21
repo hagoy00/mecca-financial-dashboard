145c87fed624ef53f0cb496782001efe91692a12
hagoy00 <hagos_y@icloud.com>
2026-04-21 11:42:29 -0700

    Update load_data function in db_utils.py
    Refactor load_data function to include docstring and modify return value.

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -1,16 +1,11 @@
-from sqlalchemy import create_engine
+
 import pandas as pd
-from utils.data_utils import load_excel
-
-def get_engine():
-    return create_engine("sqlite:///mecca_finance.db")
-
-def init_db():
-    df = load_excel()
-    engine = get_engine()
-    df.to_sql("transactions", engine, if_exists="replace", index=False)
 
 def load_data():
-    engine = get_engine()
+    """
+    Loads the MECCA Excel file as a DataFrame.
+    Assumes the file is in the project root folder.
+    """
     file_path = "MECCA_Financial_Data.xlsx"
-    return pd.read_excel(file_path, sheet_name=None)
+    df = pd.read_excel(file_path)
+    return df
