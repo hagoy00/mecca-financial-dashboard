8ffa7c8bdb52ee628448d5cc7d067400404e2e4f
hagoy00 <hagos_y@icloud.com>
2026-04-20 22:02:53 -0700

    Update db_utils.py

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -1,1 +1,16 @@
+from sqlalchemy import create_engine
+import pandas as pd
+from utils.data_utils import load_excel
+
+def get_engine():
+    return create_engine("sqlite:///mecca_finance.db")
+
+def init_db():
+    df = load_excel()
+    engine = get_engine()
+    df.to_sql("transactions", engine, if_exists="replace", index=False)
+
+def load_data():
+    engine = get_engine()
+    return pd.read_sql("SELECT * FROM transactions", engine)
 
