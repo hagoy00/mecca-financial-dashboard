3e069011b88f45ad86a1acdf88bdfa5df140cd1e
hagoy00 <hagos_y@icloud.com>
2026-04-21 12:27:09 -0700

    Update 3_Surplus_Deficit.py

--- pages/3_Surplus_Deficit.py
+++ pages/3_Surplus_Deficit.py
@@ -1,15 +1,17 @@
 import streamlit as st
 from utils.db_utils import load_data
-from utils.data_utils import extract_subtotals
+from utils.data_utils import extract_subtotals, build_board_categories, pivot_report
 
-st.title("Surplus / Deficit")
+st.title("Surplus / Deficit Report")
 
 df = load_data()
-subtotals = extract_subtotals(df)
+long_df = extract_subtotals(df)
+board_df = build_board_categories(long_df)
+pivot = pivot_report(board_df)
 
-pivot = subtotals.pivot_table(
-    index="Year", columns="Category", values="Amount", aggfunc="sum"
-)
+pivot["Surplus/Deficit"] = pivot["Total Income"] - pivot["Total Expenses"]
 
-st.dataframe(pivot)
+st.subheader("Surplus / Deficit by Year")
+st.dataframe(pivot[["Year", "Surplus/Deficit"]])
 
+st.line_chart(pivot.set_index("Year")["Surplus/Deficit"])
