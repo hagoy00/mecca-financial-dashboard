2fa088ac7fa25f025f3cf54a1d04514069592f98
hagoy00 <hagos_y@icloud.com>
2026-06-04 11:08:59 -0700

    Increase font size in financial dashboard styles

--- church_financial_subtotals_dashboard_new.py
+++ church_financial_subtotals_dashboard_new.py
@@ -69,14 +69,14 @@ html, body, div, span, p, label, h1, h2, h3, h4, h5, h6 {
     font-size: 31px !important;
 }
 .stMarkdown, .stText, .stDataFrame, .stTable, .stMetric, .stNumberInput, .stSlider {
-    font-size: 50px !important;
+    font-size: 58px !important;
     font-weight: bold !important;
 }
 .dataframe tbody tr td {
-    font-size: 50px !important;
+    font-size: 58px !important;
 }
 .dataframe thead tr th {
-    font-size: 50px !important;
+    font-size: 58px !important;
     font-weight: bold !important;
 }
 </style>
