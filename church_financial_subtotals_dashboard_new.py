b6c25f454ceecba57084f94616bdb3b82a5e81d1
hagoy00 <hagos_y@icloud.com>
2026-06-04 15:17:00 -0700

    Update church_financial_subtotals_dashboard_new.py

--- church_financial_subtotals_dashboard_new.py
+++ church_financial_subtotals_dashboard_new.py
@@ -69,14 +69,14 @@ html, body, div, span, p, label, h1, h2, h3, h4, h5, h6 {
     font-size: 30px !important;
 }
 .stMarkdown, .stText, .stDataFrame, .stTable, .stMetric, .stNumberInput, .stSlider {
-    font-size: 10px !important;
+    font-size: 90px !important;
     font-weight: bold !important;
 }
 .dataframe tbody tr td {
-    font-size: 10px !important;
+    font-size: 90px !important;
 }
 .dataframe thead tr th {
-    font-size: 10px !important;
+    font-size: 90px !important;
     font-weight: bold !important;
 }
 </style>
